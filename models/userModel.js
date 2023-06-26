5920ce92fe11fec8735c61fcc3acff02ca0f1700
Eilonasraf <103586426+Eilonasraf@users.noreply.github.com>
2023-06-26 14:23:15 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,6 @@
-// Import mongoose to interact with MongoDB and bcryptjs for password hashing
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// Define user schema for MongoDB
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -17,19 +15,13 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     }
-});
+}, { collection: 'Users' }); // Set the collection name to 'Users'
 
-// Use Mongoose's pre-save middleware to hash password before saving user document
-UserSchema.pre('save', async function(next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-    next();
-});
+// Define static methods on the User model
+UserSchema.statics.findOne = function() {
+    // Implementation of the findOne method
+};
 
-// Create the User model from the schema
 const User = mongoose.model('User', UserSchema);
 
-// Export the User model for use in other modules
 module.exports = User;
