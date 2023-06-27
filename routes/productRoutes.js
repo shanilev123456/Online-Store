53243d178718df25312e34381e148d49ec078ed3
Eilonasraf <103586426+Eilonasraf@users.noreply.github.com>
2023-06-27 15:47:15 +0300

    Add files via upload

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,10 @@ const productController = require('../controllers/productController');
 router.get('/new', productController.newProductForm);
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
+
 router.get('/:id/edit', productController.editProductForm);
+
+router.get('/:id', productController.createProduct);
 router.put('/:id', productController.updateProduct);
 router.get('/:id', productController.getProductById);
 router.delete('/:id', productController.deleteProduct);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,15 @@ const userController = require('../controllers/userController');
 router.post('/signup', userController.createUser);
 router.get('/signup', userController.signupForm);
 router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/:id', userController.getUser);
+router.get('/user', userController.login);
+
+//router.post('/logout', userController.logout);
+router.get('/:id', userController.logout);
+//router.get('/:id', userController.getUser);
+
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
+router.post('/cart/addToCart/:productId', userController.addToCart);
+router.post('/cart/checkout', userController.checkout);
 module.exports = router;
