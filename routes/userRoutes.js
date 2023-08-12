2ca2351453b7190ac79130df2f3ec2bb358f6f03
Eilonasraf <103586426+Eilonasraf@users.noreply.github.com>
2023-08-12 21:41:33 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,16 +7,14 @@ router.get('/signup', userController.signupForm);
 router.post('/login', userController.login);
 router.get('/user', userController.login);
 
-//router.post('/logout', userController.logout);
+
 router.get('/:id', userController.logout);
-//router.get('/:id', userController.getUser);
+
 
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 router.get('/checkout', userController.checkout);
 
-//router.get('/cart', userController.getCart);
-//router.post('/cart/addToCart/:productId', userController.addToCart);
-//router.get('/cart/checkout', userController.checkout);
+
 module.exports = router;
